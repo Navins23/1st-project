6c8e03cb93c4ce17815efbfd90d152e701e53554
Navin Shahi <navinshahi66@gmail.com>
2019-04-21 17:56:11 +0545

    sorted list

--- src/app/sorting/sorting.component.ts
+++ src/app/sorting/sorting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{SortserviceService} from '../sortservice.service';
- 
+ import{OrderPipe} from 'ngx-order-pipe';
 
 @Component({
   selector: 'app-sorting',
@@ -10,12 +10,27 @@ import{SortserviceService} from '../sortservice.service';
 })
 
 export class SortingComponent implements OnInit {
- sorting;
-
-  constructor(private ss:SortserviceService) { }
+  reverse:boolean=false;
+  order:string='id';
+   sorting;
+ sortedCollection:any[];
+  constructor(private ss:SortserviceService,private orderPipe:OrderPipe) {
+    this.sortedCollection = orderPipe.transform(this.sorting, 'id');
+    console.log(this.sortedCollection);
+   }
+   setOrder(value: string) {
+    if (this.order === value) {
+      this.reverse = !this.reverse;
+    }
+  
+    this.order = value;
+  }
 
   ngOnInit() {
  this.ss.display().subscribe(data=>this.sorting=data);
-}}
+ 
+}
+
+}
 
 // note: without using interface we can run the code here.
--- src/app/sortservice.service.ts
+++ src/app/sortservice.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import{HttpClient } from '@angular/common/http';
 
 
-interface Idetails{
-  name:string,class:number,roll:number,address:string  
-}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SortserviceService {
- url="https://jsonplaceholder.typicode.com/photos"
-  constructor(private http:HttpClient) { }
+
+
+ url="https://jsonplaceholder.typicode.com/users";
+ 
+  constructor(private http:HttpClient) { 
+    
+  }
   
 display(){
   return this.http.get(this.url);
 
-}}
+}
+
+}
